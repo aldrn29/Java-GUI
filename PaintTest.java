65dd5d380d1c6a46effbf062874806a9297982ea
aldrn29 <48721559+aldrn29@users.noreply.github.com>
2019-06-27 17:49:09 +0900

    Add files via upload

---
+++ PaintTest.java
@@ -0,0 +1,12 @@
+package paint;
+
+import paint.Frame;
+
+public class PaintTest {					 					 
+	public static void main(String[] arge) {
+		
+		Frame app = new Frame();  	
+		app.frameApplication();  	
+	}
+
+}
